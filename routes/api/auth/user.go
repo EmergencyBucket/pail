ba67c66b0cb630b1c5f0c2f6388d63d7f5409912
Mrxbox98 <29240392+mrxbox98@users.noreply.github.com>
2022-11-27 06:45:45 +0000

    Getuser api route

--- routes/api/auth/user.go
+++ routes/api/auth/user.go
@@ -1,7 +1,33 @@
 package auth
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/EmergencyBucket/pail/database"
+	"github.com/gin-gonic/gin"
+)
 
 func GetUser(context *gin.Context) {
+	var user database.User
 
+	strId, _ := context.Params.Get("id")
+
+	id, _ := strconv.Atoi(strId)
+
+	database.DB.First(&user, id)
+
+	if &user == nil {
+		context.JSON(http.StatusNotFound, gin.H{
+			"error": "User not found",
+		})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{
+		"id": user.ID,
+		"username": user.Username,
+		"email": user.Email,
+		"admin": user.Admin,
+	})
 }
