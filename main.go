90042f85bb5db365b901c4b65acfb26c0af6bbc4
Mrxbox98 <29240392+mrxbox98@users.noreply.github.com>
2022-11-29 03:36:24 +0000

    Ping

--- main.go
+++ main.go
@@ -4,10 +4,13 @@ import (
 	"fmt"
 
 	"github.com/EmergencyBucket/pail/database"
+	"github.com/EmergencyBucket/pail/routes"
 )
 
 func main() {
 	fmt.Println("Starting Pail v1.0.0")
 	database.Connect()
 	database.Migrate()
+
+	routes.SetupRoutes()
 }
--- routes/routes.go
+++ routes/routes.go
@@ -1,9 +1,23 @@
 package routes
 
-import "github.com/gin-gonic/gin"
-import "github.com/EmergencyBucket/pail/routes/api/auth"
+import (
+	"net/http"
+
+	"github.com/EmergencyBucket/pail/routes/api/auth"
+	"github.com/gin-gonic/gin"
+)
 
 func SetupRoutes() {
 	router := gin.Default()
 	router.GET("/api/auth/user/:id", auth.GetUser)
+	router.POST("/api/auth/user", auth.CreateUser)
+	router.GET("/api/auth/team/:id", auth.GetTeam)
+	router.POST("/api/auth/team", auth.CreateTeam)
+	router.GET("/api/auth/team/user/:id", auth.GetTeamByUser)
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	router.Run()
 }
