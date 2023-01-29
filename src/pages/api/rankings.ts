f9265a84224728f5ff4ad3db420fce6fb0db7d83
Mrxbox98 <29240392+mrxbox98@users.noreply.github.com>
2023-01-29 03:12:18 +0000

    Ranking (not tested)

--- src/pages/api/rankings.ts
+++ src/pages/api/rankings.ts
@@ -1,5 +1,6 @@
-import { PrismaClient } from '@prisma/client';
+import { Challenge, PrismaClient, Team } from '@prisma/client';
 import { NextApiRequest, NextApiResponse } from 'next';
+import { tidy, mutate, arrange, desc } from '@tidyjs/tidy';
 
 const prisma = new PrismaClient();
 
@@ -9,7 +10,71 @@ export default async function handler(
 ) {
     switch (req.method) {
         case 'GET': {
-            
+            let teams: (Team & {
+                solves: Challenge[];
+                points?: number;
+            })[] = await prisma.team.findMany({
+                include: {
+                    solves: true,
+                },
+            });
+
+            let challenges: (Challenge & {
+                solved: Team[];
+                points?: number;
+            })[] = await prisma.challenge.findMany({
+                include: {
+                    solved: true,
+                },
+            });
+
+            challenges = tidy(
+                challenges,
+                mutate({
+                    points: (
+                        challenge: Challenge & {
+                            solved: Team[];
+                        }
+                    ) =>
+                        challenge.solved.length > 150
+                            ? 200
+                            : 500 - challenge.solved.length * 2,
+                })
+            );
+
+            teams = tidy(
+                teams,
+                mutate({
+                    points: (
+                        team: Team & {
+                            solves: Challenge[];
+                            points?: number;
+                        }
+                    ) => {
+                        let points = 0;
+                        team.solves.forEach((chall) => {
+                            points += challenges.find(
+                                (challenge) => challenge.id == chall.id
+                            )?.points as number;
+                        });
+                        return points;
+                    },
+                })
+            );
+
+            teams.forEach((team) => {
+                team.secret=''
+                team.solves.forEach((challenge) => {
+                    challenge.flag=''
+                })
+            })
+
+            teams = tidy(
+                teams,
+                arrange(desc('points'))
+            )
+
+            return res.status(200).json(teams);
         }
     }
 }
