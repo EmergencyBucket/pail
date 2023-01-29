a55258199d2da7f7555b3acfcea4fee03b649baa
rklra <raj@rkalra.com>
2023-01-28 17:02:20 -0800

    new buttons

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import Link from 'next/link';
 import React from 'react';
 
@@ -14,14 +15,34 @@ const Button = ({ onClick, children, link }: Props) => {
             {link ? (
                 <Link
                     href={link}
-                    className="bg-slate-800 text-white p-2 border-4 border-slate-700 hover:border-slate-500"
+                    className={clsx(
+                        "inline-flex select-none items-center justify-center rounded-md px-4 py-2 text-sm font-medium",
+                        "bg-white text-gray-700 hover:bg-gray-50 dark:bg-gray-700 dark:text-gray-100 dark:hover:bg-gray-800",
+                        "hover:bg-gray-50",
+                        "focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75",
+                        // Register all radix states
+                        "group",
+                        "radix-state-open:bg-gray-50 dark:radix-state-open:bg-gray-900",
+                        "radix-state-on:bg-gray-50 dark:radix-state-on:bg-gray-900",
+                        "radix-state-instant-open:bg-gray-50 radix-state-delayed-open:bg-gray-50"
+                      )}
                 >
                     {children}
                 </Link>
             ) : (
                 <button
                     onClick={onClick}
-                    className="bg-slate-800 text-white p-2 border-4 border-slate-700 hover:border-slate-500"
+                    className={clsx(
+                        "inline-flex select-none items-center justify-center rounded-md px-4 py-2 text-sm font-medium",
+                        "bg-white text-gray-700 hover:bg-gray-50 dark:bg-gray-700 dark:text-gray-100 dark:hover:bg-gray-800",
+                        "hover:bg-gray-50",
+                        "focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75",
+                        // Register all radix states
+                        "group",
+                        "radix-state-open:bg-gray-50 dark:radix-state-open:bg-gray-900",
+                        "radix-state-on:bg-gray-50 dark:radix-state-on:bg-gray-900",
+                        "radix-state-instant-open:bg-gray-50 radix-state-delayed-open:bg-gray-50"
+                      )}
                 >
                     {children}
                 </button>
