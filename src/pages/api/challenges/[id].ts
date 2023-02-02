bce82d25a80cfff2d02f0b00597c02c123752821
Mrxbox98 <29240392+mrxbox98@users.noreply.github.com>
2023-02-02 23:36:48 +0000

    Add delete endpoint

--- src/pages/api/challenges/[id].ts
+++ src/pages/api/challenges/[id].ts
@@ -1,5 +1,7 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Session } from '@prisma/client';
 import { NextApiRequest, NextApiResponse } from 'next';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../auth/[...nextauth]';
 
 const prisma = new PrismaClient();
 
@@ -18,6 +20,43 @@ export default async function handler(
             });
 
             return res.status(200).json(challenge);
+        }
+        case 'DELETE': {
+            const { id } = req.query;
+
+            const session: Session | null = await getServerSession(
+                req,
+                res,
+                authOptions
+            );
+
+            if (!session) {
+                return res.status(401).json({
+                    Error: 'You must be logged in to preform this action.',
+                });
+            }
+
+            let user = await prisma.user.findFirst({
+                where: {
+                    id: session.userId
+                }
+            })
+
+            if(!user || !user.admin) {
+                return res.status(401).json({
+                    Error: 'You do not have permission to preform this action.',
+                });
+            }
+
+            await prisma.team.delete({
+                where: {
+                    id: id as string
+                }
+            })
+
+            return res.status(200).json({
+                Message: "Challenge deleted."
+            })
         }
     }
 }
