d1fb081dd193f1aaa2b161bf0106bb0704751afc
Mrxbox98 <29240392+mrxbox98@users.noreply.github.com>
2023-02-08 05:13:08 +0000

    Admin page and edit challenge component WIP

---
+++ src/components/EditChallenge.tsx
@@ -0,0 +1,118 @@
+import { Challenge } from '@prisma/client';
+import { FormEvent, useState } from 'react';
+import Button from './Button';
+import Modal from './Modal';
+
+interface Props {
+    challenge: Challenge;
+}
+
+const CreateChallenge = ({challenge}: Props) => {
+    const [open, setOpen] = useState(false);
+
+    async function submit(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+
+        await fetch(`/api/challenges`, {
+            method: 'PATCH',
+            body: JSON.stringify({
+                //@ts-ignore
+                name: event.target.name.value,
+                //@ts-ignore
+                description: event.target.description.value,
+                //@ts-ignore
+                files: event.target.files.value.split(','),
+                //@ts-ignore
+                flag: event.target.flag.value,
+                //@ts-ignore
+                category: event.target.category.value,
+            }),
+        });
+    }
+
+    return (
+        <>
+            <Modal visible={open} onClose={() => setOpen(false)}>
+                <p className="text-white text-4xl">Create a challenge</p>
+                <form onSubmit={submit}>
+                    <input
+                        type={'text'}
+                        placeholder="Challenge name"
+                        name="name"
+                        value={challenge.name}
+                        className={
+                            'bg-slate-700 border-2 border-slate-500 my-2 pl-2 w-full'
+                        }
+                    />
+                    <br />
+                    <textarea
+                        placeholder="Challenge description"
+                        name="description"
+                        value={challenge.description}
+                        className={
+                            'bg-slate-700 border-2 border-slate-500 my-2 pl-2 w-full'
+                        }
+                    />
+                    <br />
+                    <select
+                        name="category"
+                        value={challenge.category}
+                        className={
+                            'bg-slate-700 border-2 border-slate-500 my-2 pl-2 w-full'
+                        }
+                    >
+                        <option value={'WEB'}>Web</option>
+                        <option value={'CRYPTO'}>Crypto</option>
+                        <option value={'REV'}>Rev</option>
+                        <option value={'PWN'}>Pwn</option>
+                        <option value={'MISC'}>Misc</option>
+                    </select>
+                    <br />
+                    <input
+                        type={'text'}
+                        placeholder="Challenge files"
+                        name="files"
+                        value={challenge.files}
+                        className={
+                            'bg-slate-700 border-2 border-slate-500 my-2 pl-2 w-full'
+                        }
+                    />
+                    <br />
+                    <input
+                        type={'text'}
+                        placeholder="Challenge flag"
+                        name="flag"
+                        className={
+                            'bg-slate-700 border-2 border-slate-500 my-2 pl-2 w-full'
+                        }
+                    />
+                    <br />
+                    <input
+                        type={'submit'}
+                        className={
+                            'bg-slate-800 cursor-pointer text-white my-2 p border-2 w-full border-slate-700 hover:border-slate-500'
+                        }
+                    />
+                </form>
+            </Modal>
+            <Button onClick={() => setOpen(true)}>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6 mx-auto"
+                >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M12 4.5v15m7.5-7.5h-15"
+                    />
+                </svg>
+            </Button>
+        </>
+    );
+};
+
+export default CreateChallenge;
---
+++ src/pages/admin.tsx
@@ -0,0 +1,46 @@
+import Page from '@/components/Page';
+import { Challenge } from '@prisma/client';
+import { useSession } from 'next-auth/react';
+import { useEffect, useState } from 'react';
+
+export default function Home() {
+
+    const { data: session } = useSession();
+
+    const [ challenges, setChallenges ] = useState<Challenge[]>();
+
+    async function getChallenges() {
+        let req = await fetch(`/api/challenges`, {
+            method: 'GET',
+        });
+
+        let res = await req.json();
+
+        setChallenges(res);
+    }
+
+    useEffect(() => {
+        getChallenges();
+    }, [])
+
+    return (
+        <>
+            <Page title="Admin">
+                <div className='flex flex-wrap'>
+                    <div className='w-1/2'>
+                        
+                    </div>
+                    <div className='w-1/2'>
+
+                    </div>
+                    <div className='w-1/2'>
+
+                    </div>
+                    <div className='w-1/2'>
+
+                    </div>
+                </div>
+            </Page>
+        </>
+    );
+}
