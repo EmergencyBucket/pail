a2c45fbced6e7c76b9d1818f8664c2548ed041c6
Mrxbox98 <29240392+mrxbox98@users.noreply.github.com>
2023-02-18 06:50:55 +0000

    Web hosting

---
+++ src/pages/api/challenges/host/[id].ts
@@ -0,0 +1,92 @@
+import { PrismaClient } from "@prisma/client";
+import Dockerode from "dockerode";
+import { StatusCodes } from "http-status-codes";
+import isString from "is-string";
+import { CTFEnd, CTFStart } from "lib/Middleware";
+import { NextApiRequest, NextApiResponse } from "next";
+
+const prisma = new PrismaClient();
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    switch (req.method) {
+        case 'GET': {
+            if(await CTFStart(req, res, prisma)) return;
+            if(await CTFEnd(req, res, prisma)) return;
+
+            const { id } = req.query;
+
+            if(!isString(id)) {
+                return res.status(StatusCodes.BAD_REQUEST).json({
+                    Error: "Bad request."
+                })
+            }
+
+            let challenge = await prisma.challenge.findFirst({
+                where: {
+                    id: id
+                }
+            })
+
+            if(!challenge || !challenge.image) {
+                return res.status(StatusCodes.NOT_FOUND).json({
+                    Error: "Challenge not found or does not contain an image."
+                })
+            }
+
+            let host = await prisma.host.findFirst();
+
+            if(!host) {
+                return res.status(StatusCodes.SERVICE_UNAVAILABLE).json({
+                    Error: "No host available."
+                })
+            }
+
+            let availablePorts: number[] = [];
+
+            for(let i=5000; i++; i<6000) {
+                if(!host.usedPorts.includes(i)) {
+                    availablePorts.push(i);
+                }
+            }
+
+            let port = availablePorts[Math.floor(Math.random() * availablePorts.length)];
+
+            host.usedPorts.push(port);
+
+            await prisma.host.update({
+                where: {
+                    id: host.id
+                },
+                data: {
+                    usedPorts: host.usedPorts
+                }
+            })
+
+            let docker = new Dockerode({
+                host: host.remote,
+                port: host.port ?? 2375,
+            })
+
+            let container = await docker.createContainer({
+                Image: challenge.image,
+                ExposedPorts: {
+                    '80': port
+                }
+            })
+
+            // TODO: Add port freeing
+            setTimeout(() => {
+                container.kill();
+                container.remove();
+            },
+            1000 * 300)
+
+            return res.status(StatusCodes.OK).json({
+                url: host.remote+":"+port
+            })
+        }
+    }
+}
