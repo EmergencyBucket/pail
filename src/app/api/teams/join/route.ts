2f2208b8abefc8a2dfbcf1695f1b864ebee4cca3
Mrxbox98 <29240392+mrxbox98@users.noreply.github.com>
2023-02-26 20:53:42 +0000

    Join team

---
+++ src/app/api/teams/join/route.ts
@@ -0,0 +1,84 @@
+import { PrismaClient } from "@prisma/client";
+import Ajv, { JSONSchemaType } from "ajv";
+import { StatusCodes } from "http-status-codes";
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+const ajv = new Ajv();
+
+interface JoinTeamRequest {
+    secret: string;
+}
+
+const JoinTeamRequestSchema: JSONSchemaType<JoinTeamRequest> = {
+    type: 'object',
+    properties: {
+        secret: {
+            type: 'string',
+        },
+    },
+    required: ['secret'],
+};
+
+const joinTeamRequestValidator = ajv.compile(JoinTeamRequestSchema);
+
+export async function POST(req: Request) {
+    const session = await getServerSession();
+
+    if (!session) {
+        return NextResponse.json({
+            Error: 'You must be logged in to preform this action.',
+        }, {
+            status: StatusCodes.UNAUTHORIZED
+        });
+    }
+
+    const data = await req.json();
+
+    if (!joinTeamRequestValidator(data)) {
+        return NextResponse.json({
+            Error: 'Bad request.',
+        }, {
+            status: StatusCodes.BAD_REQUEST
+        });
+    }
+
+    const user = await prisma.user.findFirst({
+        where: {
+            id: session.user?.name!
+        }
+    })
+
+    const team = await prisma.team.findFirst({
+        where: {
+            secret: data.secret,
+        },
+    });
+
+    if (!team) {
+        return NextResponse.json({
+            Error: 'Bad secret.',
+        }, {
+            status: StatusCodes.FORBIDDEN
+        });
+    }
+
+    await prisma.team.update({
+        where: {
+            secret: data.secret,
+        },
+        data: {
+            members: {
+                connect: {
+                    id: user!.id
+                },
+            },
+        },
+    });
+
+    return NextResponse.json(team, {
+        status: StatusCodes.OK
+    });
+}
--- types.d.ts
+++
@@ -1,9 +0,0 @@
-import type { DefaultUser } from 'next-auth';
-
-declare module 'next-auth' {
-    interface Session {
-        user?: DefaultUser & {
-            id: string;
-        };
-    }
-}
