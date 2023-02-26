a9c4250b83d53ddb635ab538c73e124486a48516
Mrxbox98 <29240392+mrxbox98@users.noreply.github.com>
2023-02-26 21:40:27 +0000

    Leave team

---
+++ src/app/api/teams/leave/route.ts
@@ -0,0 +1,52 @@
+import { PrismaClient } from "@prisma/client";
+import { StatusCodes } from "http-status-codes";
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+export async function POST() {
+    const session = await getServerSession();
+
+    if (!session) {
+        return NextResponse.json({
+            Error: 'You must be logged in to preform this action.',
+        }, {
+            status: StatusCodes.UNAUTHORIZED
+        });
+    }
+
+    const user = await prisma.user.findFirst({
+        where: {
+            name: session.user?.name,
+        },
+    });
+
+    let team = await prisma.team.update({
+        include: {
+            members: true,
+        },
+        where: {
+            id: user!.teamId as string,
+        },
+        data: {
+            members: {
+                disconnect: {
+                    id: user!.id,
+                },
+            },
+        },
+    });
+
+    if (team.members.length === 0) {
+        await prisma.team.delete({
+            where: {
+                id: team.id,
+            },
+        });
+    }
+
+    return NextResponse.json(team, {
+        status: StatusCodes.OK
+    });
+}
