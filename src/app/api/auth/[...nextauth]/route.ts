36288d6584f21fa514f3a8d34c68c4b7260fecc7
Mrxbox98 <29240392+mrxbox98@users.noreply.github.com>
2023-02-26 01:03:55 +0000

    Start api migration

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,28 @@
+import NextAuth, { NextAuthOptions } from 'next-auth';
+import GitHubProvider from 'next-auth/providers/github';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import prisma from 'lib/prismadb';
+
+export const authOptions: NextAuthOptions = {
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        GitHubProvider({
+            clientId: process.env.GITHUB_CLIENT_ID!,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+        }),
+    ],
+    callbacks: {
+        session: async ({ session, user }) => {
+            return {
+                ...session,
+                user: user,
+            };
+        },
+    },
+    session: {
+        strategy: 'jwt',
+    },
+};
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
