92da2fd36a1f9bbc3236f6ca6053ee176fe43422
Mrxbox98 <29240392+mrxbox98@users.noreply.github.com>
2023-02-26 19:45:52 +0000

    Setting

---
+++ src/app/api/settings/route.ts
@@ -0,0 +1,93 @@
+import { PrismaClient } from "@prisma/client";
+import Ajv, { JSONSchemaType } from "ajv";
+import { StatusCodes } from "http-status-codes";
+import { admin } from "lib/Middleware";
+import { NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+const ajv = new Ajv();
+
+interface SettingRequest {
+    key: string;
+    value: string;
+    pub: boolean;
+}
+
+const SettingRequestSchema: JSONSchemaType<SettingRequest> = {
+    type: 'object',
+    properties: {
+        key: { type: 'string' },
+        value: { type: 'string' },
+        pub: { type: 'boolean' },
+    },
+    required: ['key', 'value', 'pub'],
+};
+
+const settingRequestValidator = ajv.compile(SettingRequestSchema);
+
+export async function GET() {
+    let temp;
+    if ((temp = await admin(prisma))) {
+        return temp;
+    }
+
+    let settings = await prisma.setting.findMany();
+
+    return NextResponse.json(settings, {
+        status: StatusCodes.OK
+    });
+}
+
+export async function POST(req: Request) {
+    let temp;
+    if ((temp = await admin(prisma))) {
+        return temp;
+    }
+
+    let reqsetting = await req.json();
+
+    if (settingRequestValidator(reqsetting)) {
+        const { key, value, pub } = reqsetting;
+
+        let curr = await prisma.setting.findFirst({
+            where: {
+                key: key,
+            },
+        });
+
+        if (curr) {
+            await prisma.setting.update({
+                where: {
+                    key: key,
+                },
+                data: {
+                    value: value,
+                    public: pub,
+                },
+            });
+
+            return NextResponse.json(curr, {
+                status: StatusCodes.OK
+            });
+        }
+
+        let setting = await prisma.setting.create({
+            data: {
+                key: key,
+                value: value,
+                public: pub,
+            },
+        });
+
+        return NextResponse.json(setting, {
+            status: StatusCodes.CREATED
+        });
+    } else {
+        return NextResponse.json({
+            Error: 'Bad request.',
+        }, {
+            status: StatusCodes.BAD_REQUEST
+        });
+    }
+}
