9f99c3fbf940c9f2bf7290912288dccc5896ac1c
Mrxbox98 <29240392+mrxbox98@users.noreply.github.com>
2023-03-14 19:49:22 +0000

    Title fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,55 +1,15 @@
-'use client';
-
+import Bucket from '@/components/Bucket';
 import Image from 'next/image';
 import Link from 'next/link';
-import { useEffect } from 'react';
-import bucket from '../../public/bucket.json';
+
+export const metadata = {
+    title: 'EBucket | Home',
+};
 
 export default function Home() {
-    async function load() {
-        let c = document.getElementById('canvas');
-        //  @ts-ignore
-        let ctx = c.getContext('2d');
-
-        for (let x = 0; x < bucket.length; x++) {
-            let sl =
-                200 /
-                bucket[x].filter((b) => {
-                    return b[3];
-                }).length;
-            for (let y = bucket[x].length - 1; y >= 0; y--) {
-                setTimeout(() => {
-                    ctx.beginPath();
-                    if (bucket[x][y][3]) {
-                        ctx.strokeStyle = `#1d1f20`;
-                        ctx.lineWidth = '0.5';
-                        ctx.rect(x * 5 - 0.5, y * 5 - 0.5, 5, 5);
-                    }
-
-                    ctx.fillStyle = `rgba(${bucket[x][y][0]}, ${bucket[x][y][1]}, ${bucket[x][y][2]}, ${bucket[x][y][3]})`;
-                    ctx.fillRect(x * 5, y * 5, 4.5, 4.5);
-                    ctx.stroke();
-                }, sl * y);
-            }
-        }
-    }
-
-    useEffect(() => {
-        load();
-    }, []);
-
     return (
         <>
-            <head>
-                <title>EBucket | Home</title>
-            </head>
-            <canvas
-                id="canvas"
-                className="mx-auto"
-                style={{ zIndex: 100 }}
-                height={320}
-                width={320}
-            />
+            <Bucket />
             <h1 className="text-6xl font-bold mx-auto text-center text-white">
                 BucketCTF<i>[2023]</i>
             </h1>
