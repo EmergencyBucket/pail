17f95975095f908fa9f7059662a40ff4e5ccb1f3
Mrxbox98 <29240392+mrxbox98@users.noreply.github.com>
2023-04-03 17:35:36 +0000

    Remove prisma global

--- src/lib/prismadb.ts
+++ src/lib/prismadb.ts
@@ -1,11 +1,5 @@
 import { PrismaClient } from '@prisma/client';
 
-declare global {
-    // eslint-disable-next-line
-    var prisma: PrismaClient;
-}
-
-const prisma = globalThis.prisma || new PrismaClient();
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma;
+const prisma = new PrismaClient();
 
 export default prisma;
