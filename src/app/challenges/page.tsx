13d7b914f81ee634623bce901a7e824568246d50
Mrxbox98 <29240392+mrxbox98@users.noreply.github.com>
2023-04-07 16:59:31 +0000

    a

--- src/app/challenges/page.tsx
+++ src/app/challenges/page.tsx
@@ -82,11 +82,18 @@ export default async function Home({
     });
 
     challenges.forEach((challenge) => {
-        challenge.points = challenge.staticPoints
-            ? challenge.staticPoints
-            : challenge.solved!.length > 150
-            ? 200
-            : 500 - challenge.solved!.length * 2;
+        challenge.points = Math.max(
+            200,
+            challenge.staticPoints
+                ? challenge.staticPoints
+                : 500 -
+                      challenge.solved!.length *
+                          (challenge.difficulty == Difficulty.EASY
+                              ? 4
+                              : challenge.difficulty == Difficulty.MEDIUM
+                              ? 3
+                              : 2)
+        );
     });
 
     let challengesWithoutSecrets = challenges.map((chall) =>
