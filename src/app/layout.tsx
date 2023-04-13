274de89a65fcdc504a7d3e9941cd958b0a7f3bed
Mrxbox98 <29240392+mrxbox98@users.noreply.github.com>
2023-04-12 18:27:26 -0700

    Remove notice

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,10 +12,6 @@ export default async function RootLayout({
                 <link rel="icon" type="image/x-icon" href="/bucket.png"></link>
             </head>
             <body className="w-full h-full bg-gray-900 p-8">
-                <code className="text-white text-3xl text-center">
-                    BucketCTF 2023 is OVER. If you are on the top 5 teams create
-                    a ticket in our Discord.
-                </code>
                 {/* @ts-expect-error Server Component */}
                 <Navbar />
                 {children}
