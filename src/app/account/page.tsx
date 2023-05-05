01ad3cf4cc822de46241c91b75a20f324ead44c9
Mrxbox98 <29240392+mrxbox98@users.noreply.github.com>
2023-05-05 09:10:12 -0700

    prisma

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@/components/Button';
 import { Input } from '@/components/Input';
 import { getUser } from '@/lib/Utils';
+import prisma from '@/lib/prismadb';
 
 async function submitJoin(data: FormData) {
     'use server';
