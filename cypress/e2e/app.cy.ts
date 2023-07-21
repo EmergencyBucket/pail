4fb7abdf98b3dee64f4f8602d266a8197ba7d4f6
mrxbox98 <29240392+mrxbox98@users.noreply.github.com>
2023-07-21 05:28:10 -0700

    Start of customization

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -1,9 +1,13 @@
 describe('Homepage', () => {
-    it('Has our navbar', () => {
+    it('Has our navbar home', () => {
         cy.visit('http://localhost:3000/')
 
         cy.get('a').contains('Home').parents().should('have.attr', 'href', '/');
 
         cy.get('a').contains('Challenges').parents().should('have.attr', 'href', '/challenges');
+
+        cy.get('a').contains('Rankings').parents().should('have.attr', 'href', '/rankings');
+
+        cy.get('a').contains('Sign in').parents().should('have.attr', 'href', '/api/auth/signin');
     })
 })
