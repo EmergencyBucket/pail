0a0b1167b6d3962110c6c39f3c171d3b44b8e3bd
mrxbox98 <29240392+mrxbox98@users.noreply.github.com>
2023-07-23 07:40:19 -0700

    Fixes

--- docs/next.config.js
+++ docs/next.config.js
@@ -3,7 +3,11 @@ const withNextra = require('nextra')({
     themeConfig: './theme.config.jsx',
 })
 
-module.exports = withNextra()
+module.exports = withNextra({
+  images: {
+    unoptimized: true,
+  }
+})
 
   // If you have other Next.js configurations, you can pass them as the parameter:
   // module.exports = withNextra({ /* other next.js config */ })
